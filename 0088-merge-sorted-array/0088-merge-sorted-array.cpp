9a5db11bad40a5afc5ffffaa7a044690f8ccac4c
afroj-quant <afrojk6033@gmail.com>
2026-08-09 15:25:58 +0530

    Time: 0 ms (100%), Space: 12.5 MB (31.71%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=0; i<n;i++){
-            nums1[m] = nums2[i];
-            m++;
+        int idx = m+n-1 , i = m-1 , j = n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>=nums2[j]){
+                nums1[idx] = nums1[i];
+                idx--, i--;
+            }
+            else{
+                nums1[idx] = nums2[j];
+                idx--, j--;
+            }
         }
-        sort(nums1.begin(),nums1.end());
-        for(int i=0; i<m;i++){
-            cout<<nums1[i];
+        while(j>=0){
+            nums1[idx] = nums2[j];
+            idx--, j--;
         }
-
+        
     }
 };
