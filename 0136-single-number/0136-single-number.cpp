0b4a2d53d586710bb8f7e030b1c07ab2819bcfeb
afroj-quant <afrojk6033@gmail.com>
2026-08-09 15:32:38 +0530

    Time: 0 ms (100%), Space: 20.7 MB (24.24%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
+        int x = 0;
         for(int i=0; i<nums.size(); i++){
-            ans ^= nums[i];
+            x ^= nums[i];
         }
-        return ans;
+        return x;
     }
 };
